9fba795333ab8607dd19ca54022fe69ee84ace7a
Kishan Kulkarni <kulkarnikishan610@gmail.com>
2024-04-22 02:28:18 +0530

    CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = require("./allowedOrigins");
 
 let corsOptions = {
   origin: (origin, callback) => {
+    console.log(origin);
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
